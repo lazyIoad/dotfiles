966be1c836937609da28d936cef998a7dbc28db3
Areeb Khan <arekhan@linkedin.com>
2023-03-13 17:27:47 -0400

    refactor a bit

--- flake.nix
+++ flake.nix
@@ -9,20 +9,18 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }: let
-    withCfg = { arch, homeDirectory} : 
-      home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.${arch};
-        modules = [ (import ./home.nix homeDirectory) ];
-      };
-  in {
+  outputs = { nixpkgs, home-manager, ... }: 
+  {
     defaultPackage = {
       x86_64-darwin = home-manager.defaultPackage.x86_64-darwin;
       aarch64-darwin = home-manager.defaultPackage.aarch64-darwin;
     };
 
     homeConfigurations = {
-      "areeb" = withCfg { arch = "x86_64-darwin"; homeDirectory = "/Users/areeb"; };
+      "lazyload" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages."x86_64-darwin";
+        modules = [ (import ./hosts/home.nix  "/Users/areeb") ];
+      };
     };
   };
 }
