27646e9e7aa4683f6ae7a9bc0f9a33b7437d9bfa
lazyload <git@arb.computer>
2023-03-17 18:36:45 -0400

    add lazyvim

---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,13 @@
+return {
+  { "catppuccin", enabled = false },
+  { "neoconf.nvim", enabled = false },
+  { "neodev.nvim", enabled = false },
+  -- { "noice.nvim", enabled = false },
+  -- { "nui.nvim", enabled = false },
+  { "nvim-navic", enabled = false },
+  -- { "nvim-notify", enabled = false },
+  { "persistence.nvim", enabled = false },
+  { "todo-comments.nvim", enabled = false },
+  { "trouble.nvim", enabled = false },
+  { "vim-startuptime", enabled = false },
+}
