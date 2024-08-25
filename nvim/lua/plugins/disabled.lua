7a05d703e623cc24da732c1d18743fd34f9eb69e
lazyload <git@lazyloading.net>
2024-08-25 16:16:26 -0400

    disable a bunch of lazy plugins

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,3 +1,11 @@
 return {
+  { "Bilal2453/luvit-meta", enabled = false },
+  { "catppuccin/nvim", enabled = false },
+  { "folke/lazydev.nvim", enabled = false },
+  { "folke/tokyonight.nvim", enabled = false },
+  { "linux-cultist/venv-selector.nvim", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
+  { "saecki/crates.nvim", enabled = false },
+  { "stevearc/dressing.nvim", enabled = false },
 }
